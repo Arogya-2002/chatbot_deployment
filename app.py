7d4beb13a8a9e620f643b4ccf8466717c4f66fd4
Arogya-2002 <arogyavamshi2002@gmail.com>
2025-02-20 15:11:29 +0530

    git actions file added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import gradio as gr
 
-import os
+import sys
 from src.Exceptions import CustomException
 from src.Logs import logging
 from src.components.pdf_utils import PdfExtraction
@@ -11,37 +11,31 @@ class GradioInterface:
         pass
 
     def chat_with_resume_qa(self, pdf_file, question, history):
-        """
-        Handles the user's question and answers based on the uploaded resume.
+        try:
+            logging.info("The chat has started")
 
-        Args:
-            pdf_file (file): The uploaded resume PDF.
-            question (str): The user's question about the resume.
-            history (list): Conversation history.
-
-        Returns:
-            str: The assistant's answer based on the resume.
-            list: Updated history.
-        """
-        # Ensure history is a list (if None, initialize as an empty list)
-        history = history or []
-
-        pe = PdfExtraction()
-
-        # Extract the text from the PDF
-        resume_text = pe.extract_text_from_pdf(pdf_file.name)
-
-        if "Error" in resume_text or "No extractable text found" in resume_text:
-            print(f"PDF extraction error: {resume_text}")  # Log extraction error
-            return resume_text, history
+            # Ensure history is a list (if None, initialize as an empty list)
+            history = history or []
         
-        ol = OllamaClient()
+            pe = PdfExtraction()
+            logging.info("PdfExtraction object created successfully")
 
-        # Call the function to generate an answer based on the resume
-        response, history = ol.answer_resume_question(question, resume_text, history)
+            # Extract the text from the PDF
+            resume_text = pe.extract_text_from_pdf(pdf_file.name)
+        
 
-        return response, history
+            if "Error" in resume_text or "No extractable text found" in resume_text:
+                print(f"PDF extraction error: {resume_text}")  # Log extraction error
+                return resume_text, history
+        
+            ol = OllamaClient()
+            logging.info("OllamaClient object created successfully")
 
+            # Call the function to generate an answer based on the resume
+            response, history = ol.answer_resume_question(question, resume_text, history)
+            return response, history
+        except Exception as e:
+            raise CustomException(e,sys)
 
 gi = GradioInterface()
 
